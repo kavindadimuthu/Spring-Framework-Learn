b9c5d48779ad8a5d370204f94a2b9a8b79aabab8
kavindadimuthu <kavindadimuthu260@gmail.com>
2025-02-09 21:33:43 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v/user")
+@CrossOrigin
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUser")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+}
